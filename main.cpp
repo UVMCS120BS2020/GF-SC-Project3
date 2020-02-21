74ecd917e372be06bc3201da791c8fe94917e790
Sean Cosgrove <sean.m.cosgrove@uvm.edu>
2020-02-21 15:44:16 -0500

    reorganized main() function into three global void functions to support destroyerBattle() function; startGame(), battleshipGame(), and destroyerGame()

--- main.cpp
+++ main.cpp
@@ -16,58 +16,86 @@ using namespace std;
 
 /********** Global functions declarations **********/
 
+// start game
+// Requires: nothing
+// Modifies: nothing
+// Effects: gets user input for battleship or destroyer battle, calls respective game function
+void startGame();
+
+// battleship game
+// Requires: nothing
+// Modifies: battleship object
+// Effects: calls make user battleship and battle functions
+void battleshipGame();
+
+// destroyer game
+// Requires: nothing
+// Modifies: destroyer object
+// Effects: calls make user destroyer and destroyer battle function
+void destroyerGame();
+
 // make user battleship
 // Requires: nothing
-// modifies: creates a new battleship with user input values
-// effects: returns a new battleship
+// Modifies: creates a new battleship with user input values
+// Effects: returns a new battleship
 Battleship makeUserBattleship();
 
 // make user destroyer
 // Requires: nothing
-// modifies: creates a new destroyer with user input values
-// effects: returns a new destroyer
+// Modifies: creates a new destroyer with user input values
+// Effects: returns a new destroyer
 Destroyer makeUserDestroyer();
 
 // Requires: two battleships
-// modifies: hitpoints of the battleships
-// effects: fires until one is destroyed (takes turns, battleship one goes first). returns the winner.
+// Modifies: hitpoints of the battleships
+// Effects: fires until one is destroyed (takes turns, battleship one goes first). returns the winner.
 Battleship battle(Battleship &pOne, Battleship &pTwo);
 
 // Requires: two destroyers
-// modifies: hitpoints of the battleships
-// effects: fires until one is destroyed (takes turns, destroyer one goes first). returns the winner.
+// Modifies: hitpoints of the battleships
+// Effects: fires until one is destroyed (takes turns, destroyer one goes first). returns the winner.
 Destroyer destroyerBattle(Destroyer &pOne, Destroyer &pTwo);
 
 // MAIN
-int main(){
+int main() {
+    startGame();
+    return 0;
+}
 
-    // initialize some variables
-    bool userDestroyer;
+/********** Global function definitions **********/
+
+// startGame function
+void startGame() {
+    // get user input
     char choice;
-    /*cout << "Do you want to deploy a Destroyer Battleship? (Y/N) ";
+    cout << "Battleship or Destroyer battle? (B/D) ";
     cin >> choice;
-    while (tolower(choice) != 'y' && tolower(choice) != 'n') {
-        cout << "Please enter a 'Y' or 'N'";
+    while (tolower(choice) != 'b' && tolower(choice) != 'd') {
+        cout << "Please enter a 'B' or 'D' ";
         cin >> choice;
     }
-
-    if (tolower(choice) == 'y') {
-        Destroyer userShip = makeUserDestroyer();
-        userDestroyer = true;
-    } else if (tolower(choice) == 'n') {
-        Battleship userShip = makeUserBattleship();
-        userDestroyer = false;
+    // user wants to fight with destroyers
+    if (tolower(choice) == 'D') {
+        destroyerGame(); // call destroyerGame function
+    // user wants to fight with battleships
+    } else if (tolower(choice) == 'B') {
+        battleshipGame(); // call battleshipGame function
     } else {
-        Battleship userShip = makeUserBattleship();
-        userDestroyer = false;
+        battleshipGame();
     }
-    */
+}
+
+// battleshipGame function
+void battleshipGame() {
+    // initialize variables
     Battleship userShip = makeUserBattleship();
     Board board = Board();
     int userX;
     int userY;
     int counter = 0;
     bool forward;
+    char choice;
+    // while loop until game is over
     while (userShip.getDestroyed() == false){
         // if initial position
         if (userShip.getX()) {
@@ -142,21 +170,13 @@ int main(){
         if (choice == 'Y' || choice == 'y' ){
             // count how many ships
             ++counter;
-            if (userDestroyer) {
-                unique_ptr<Destroyer> computerPtr = make_unique<Destroyer>("Computer " + to_string(counter));
-                // computer position
-                computerPtr->setX(0);
-                computerPtr->setY(0);
-                cout << "Your distance from " << computerPtr->getName() << " is " << board.calcDistance(userShip, *computerPtr) << endl;
-                //destroyerBattle(userShip, *computerPtr);
-            } else {
-                unique_ptr<Battleship> computerPtr = make_unique<Battleship>("Computer " + to_string(counter));
-                // computer position
-                computerPtr->setX(0);
-                computerPtr->setY(0);
-                cout << "Your distance from " << computerPtr->getName() << " is " << board.calcDistance(userShip, *computerPtr) << endl;
-                battle(userShip, *computerPtr);
-            }
+            unique_ptr<Battleship> computerPtr = make_unique<Battleship>("Computer " + to_string(counter));
+            // computer position
+            computerPtr->setX(0);
+            computerPtr->setY(0);
+            cout << "Your distance from " << computerPtr->getName() << " is " << board.calcDistance(userShip, *computerPtr) << endl;
+            // call battle function
+            battle(userShip, *computerPtr);
         } else if(choice == 'n' || choice != 'N') {
             cout << "you're a wimp" << endl;
         } else {
@@ -164,10 +184,108 @@ int main(){
         }
     }
     cout << "You were destroyed. Game Over." << endl;
-    return 0;
 }
 
-/********** Global function definitions **********/
+// destroyerGame function
+void destroyerGame() {
+    // initialize variables
+    Destroyer userShip = makeUserDestroyer();
+    Board board = Board();
+    int userX;
+    int userY;
+    int counter = 0;
+    bool forward;
+    char choice;
+    // while loop until game is over
+    while (userShip.getDestroyed() == false){
+        // if initial position
+        if (userShip.getX()) {
+            // if they want to move on X
+            choice = 'a';
+            cout << "Do you want to move on the X axis (y/n)? ";
+            cin >> choice;
+            if (choice == 'Y' || choice == 'y') {
+                // move forward?
+                choice = 'a';
+                cout << "Do you want to move forward? (y/n) ";
+                cin >> choice;
+                if (choice == 'Y' || choice == 'y') {
+                    forward = true;
+                    cout << "your new X position is: " << board.moveX(userShip, forward) << endl;
+                } else if (choice == 'n' || choice != 'N') {
+                    forward = false;
+                    cout << "your new X position is: " << board.moveX(userShip, forward) << endl;
+                } else {
+                    cout << "thats not a y or an n" << endl;
+                }
+            } else if (choice == 'n' || choice != 'N') {
+                cout << "Ok, you did not move." << endl;
+            } else {
+                cout << "thats not a y or an n" << endl;
+            }
+        } else{
+            cout << "Enter your starting X position between 0 and " << board.getSizeX() << ": ";
+            cin >> userX;
+            while (userX > board.getSizeX() || userX < 0){
+                cout << "Enter a valid X: ";
+                cin >> userX;
+            }
+            userShip.setX(userX);
+        }
+        if (userShip.getY()){
+            // move on Y
+            choice = 'a';
+            cout << "Do you want to move on the Y axis (y/n)? ";
+            cin >> choice;
+            if (choice == 'Y' || choice == 'y') {
+                // move up?
+                choice = 'a';
+                cout << "Do you want to move up? (y/n) ";
+                cin >> choice;
+                if (choice == 'Y' || choice == 'y') {
+                    forward = true;
+                    cout << "your new Y position is: " << board.moveY(userShip, forward) << endl;
+                } else if (choice == 'n' || choice != 'N') {
+                    forward = false;
+                    cout << "your new Y position is: " << board.moveY(userShip, forward) << endl;
+                } else {
+                    cout << "thats not a y or an n" << endl;
+                }
+            } else if (choice == 'n' || choice != 'N') {
+                cout << "Ok, you did not move." << endl;
+            } else {
+                cout << "thats not a y or an n" << endl;
+            }
+        } else {
+            cout << "Enter your starting Y position between 0 and " << board.getSizeY() << ": ";
+            cin >> userY;
+            while (userY > board.getSizeY() || userY < 0){
+                cout << "Enter a valid Y: ";
+                cin >> userY;
+            }
+            userShip.setY(userY);
+        }
+        cout << "Battle enemy ship? (Y/N) ";
+        // battle the enemy
+        cin >> choice;
+        if (choice == 'Y' || choice == 'y' ){
+            // count how many ships
+            ++counter;
+            unique_ptr<Destroyer> computerPtr = make_unique<Destroyer>("Computer " + to_string(counter));
+            // computer position
+            computerPtr->setX(0);
+            computerPtr->setY(0);
+            cout << "Your distance from " << computerPtr->getName() << " is " << board.calcDistance(userShip, *computerPtr) << endl;
+            // call destroyerBattle function
+            destroyerBattle(userShip, *computerPtr);
+        } else if(choice == 'n' || choice != 'N') {
+            cout << "you're a wimp" << endl;
+        } else {
+            cout << "thats not a y or an n" << endl;
+        }
+    }
+    cout << "You were destroyed. Game Over." << endl;
+}
 
 // make user battleship function
 Battleship makeUserBattleship() {
@@ -177,7 +295,7 @@ Battleship makeUserBattleship() {
     int accuracy;
     int speed;
     cout << "Enter the name of your Battleship: ";
-    cin >> name;
+    getline(cin, name);
     cout << "Enter the Hitpoints of your Battleship (Integer Between 100-30,000): ";
     cin >> hitPoints;
     if (hitPoints < 100 || hitPoints > 30000) {
@@ -202,6 +320,7 @@ Battleship makeUserBattleship() {
         cout << "Invalid pSpeed, set to default (50)" << endl;
         speed = 50;
     }
+    // make Battleship a unique pointer and return it to battleshipGame function
     unique_ptr<Battleship> userShip = make_unique<Battleship>(name, hitPoints, firePower, accuracy, speed);
     return *userShip;
 }
@@ -214,7 +333,7 @@ Destroyer makeUserDestroyer() {
     int accuracy;
     int speed;
     cout << "Enter the name of your Battleship: ";
-    cin >> name;
+    getline(cin, name);
     cout << "Enter the Hitpoints of your Battleship (Integer Between 100-30,000): ";
     cin >> hitPoints;
     if (hitPoints < 100 || hitPoints > 30000) {
@@ -239,6 +358,7 @@ Destroyer makeUserDestroyer() {
         cout << "Invalid pSpeed, set to default (50)" << endl;
         speed = 50;
     }
+    // make Destroyer a unique pointer and return it to destroyerGame function
     unique_ptr<Destroyer> userShip = make_unique<Destroyer>(name, hitPoints, firePower, accuracy, speed);
     return *userShip;
 }
